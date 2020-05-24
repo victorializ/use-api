72baf4209f1402afa9a295f61feca73478f6dd38
Victoria Lizeikina <vlize@softserveinc.com>
2020-05-25 01:40:07 +0300

    improve readabillity

--- example/src/App.js
+++ example/src/App.js
@@ -6,14 +6,28 @@ import './App.css';
 
 function App() {
     const [response, request] = useApi(api.getUsers);
+
+    const getFirstUser = () => {
+        return request(api.getUserById, 1);
+    };
+
+    const createUser = () => {
+        request(api.newUser, null, 'user');
+    };
+
+    const generateError = () => {
+        return request(api.error);
+    };
+
     return(
         <>
             { response.loading && <div>Loading...</div> }
             { response.error && <div>{response.error}</div> }
             { response.data && <div>{JSON.stringify(response.data)}</div> }
-            <button onClick={() => request(api.getUserById, 1)}>Get First User</button>
-            <button onClick={() => request(api.newUser, null, 'user')}>New User</button>
-            <button onClick={() => request(api.error)}>Error</button>
+
+            <button onClick={getFirstUser}>Get First User</button>
+            <button onClick={createUser}>New User</button>
+            <button onClick={generateError}>Error</button>
         </>
     );
 }
